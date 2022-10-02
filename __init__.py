3f33e3afc1d4ecd18b2623d4e88a33aa33235dc7
ymma98 <767615400@qq.com>
2022-10-02 13:30:33 +0800

    fix __init__.py bug: .ova -> .OVA

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from .ova import calc_co_metric
-from .ova import calc_contra_metric
-from .ova import CoordinateSystem
+from .OVA import calc_co_metric
+from .OVA import calc_contra_metric
+from .OVA import CoordinateSystem
 
 __all__=["calc_co_metric",
          "calc_contra_metric",
